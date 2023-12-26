8fce3252620996a65418fca261fd309ef4832f29
Yusuke710 <ymiy0005@student.monash.edu>
2023-12-27 02:59:57 +1100

    initial commit

--- index.js
+++ index.js
@@ -36,3 +36,84 @@ const headerLogoConatiner = document.querySelector('.header__logo-container')
 headerLogoConatiner.addEventListener('click', () => {
   location.href = 'index.html'
 })
+
+
+// added by Yusuke 26/12/2023
+// JavaScript code for the game logic
+
+// Function to get a random integer between min and max (inclusive)
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+// Secret word and list of words
+const words = ["artificial", "intelligence", "algorithm", "text", "context", "calculate", "similarity"];
+
+// Use the current date as a seed for randomness
+const currentDate = new Date();
+const seed = currentDate.getDate(); // You can use other parts of the date for more randomness if needed
+
+// Set a seed for the random number generator
+Math.seedrandom(seed);
+
+// Get a random index to pick a secret word
+const randomIndex = getRandomInt(0, words.length - 1);
+const secretWord = words[randomIndex];
+
+// Calculate distances from the secret word
+const distances = words.map(word => ({
+    word,
+    distance: calculateDistance(secretWord, word)
+}));
+
+// Sort words based on distances
+distances.sort((a, b) => a.distance - b.distance);
+
+// Function to calculate distance between two words (for demonstration purposes)
+function calculateDistance(word1, word2) {
+    // Implement your distance calculation logic here (for example, Levenshtein distance)
+    // This is a simple placeholder function
+    return Math.abs(word1.length - word2.length);
+}
+
+// User attempt history
+const attemptHistory = [];
+
+// Function to submit a guess
+function submitGuess() {
+    const guess = document.getElementById('guessInput').value.toLowerCase();
+
+    if (words.includes(guess)) {
+        const position = words.indexOf(guess) + 1;
+        if (guess === secretWord) {
+            displayResult(`Congratulations! You found the secret word.`);
+        } else {
+            displayResult(`Your word "${guess}" is at position ${position}. Keep guessing!`);
+        }
+    } else {
+        // Display the failure attempt along with the closest words
+        const closestWords = distances.slice(0, 3).map(item => item.word);
+        displayResult(`Sorry, '${guess}' is not in the list. Your closest attempts: ${closestWords.join(', ')}. Try again.`);
+    }
+
+    // Display attempt history
+    displayAttemptHistory();
+}
+
+// Function to display the result
+function displayResult(message) {
+    document.getElementById('result').textContent = message;
+}
+
+// Function to display attempt history
+function displayAttemptHistory() {
+    const historyList = document.getElementById('attemptHistory');
+    historyList.innerHTML = ''; // Clear previous history
+
+    attemptHistory.forEach(attempt => {
+        const listItem = document.createElement('li');
+        listItem.textContent = `Word: ${attempt.word}, Position: ${attempt.position}`;
+        historyList.appendChild(listItem);
+    });
+
+  }
---
+++ text_embed.py
@@ -0,0 +1,70 @@
+from bs4 import BeautifulSoup
+import re
+import json
+
+import numpy as np  # Assuming you're using a library like NumPy for vector operations
+#import openai
+
+def scrape_and_embed_words(html_file_path):
+    try:
+        # Open and read the HTML file
+        with open(html_file_path, 'r', encoding='utf-8') as file:
+            html_content = file.read()
+
+        # Create a BeautifulSoup object to parse the HTML
+        soup = BeautifulSoup(html_content, 'html.parser')
+
+        # Extract text content from the HTML
+        text_content = soup.get_text()
+
+        # Use regular expressions to find words (ignoring case)
+        words = re.findall(r'\b\w+\b', text_content.lower())
+
+        # Create a set to store unique words
+        unique_words = set(words)
+
+        # Placeholder for word embeddings (replace this with your actual encoding logic)
+        # Assuming you have a function get_word_embedding(word) that returns the embedding for a word
+        word_embeddings = {word: get_word_embedding(word) for word in unique_words}
+
+        return word_embeddings
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return None
+
+def normalize_vector(vector):
+    magnitude = np.linalg.norm(vector)
+    if magnitude > 0:
+        return vector / magnitude
+    else:
+        return vector
+    
+def get_word_embedding(word):
+    #TODO
+    #embedding = openai.Embedding.create(input = [text], model="text-embedding-ada-002")['data'][0]['embedding']
+    embedding = np.random.rand(300)  # Example: Generating a random vector of length 300
+
+    # Normalize the vector
+    normalized_embedding = normalize_vector(embedding)
+
+    return normalized_embedding
+
+def save_word_embeddings_to_file(word_embeddings, output_file):
+    try:
+        # Convert NumPy arrays to Python lists before saving to JSON
+        word_embeddings_serializable = {word: emb.tolist() for word, emb in word_embeddings.items()}
+
+        with open(output_file, 'w', encoding='utf-8') as json_file:
+            json.dump(word_embeddings_serializable, json_file, ensure_ascii=False, indent=4)
+        print(f"Word embeddings saved to {output_file}")
+    except Exception as e:
+        print(f"An error occurred while saving word embeddings: {e}")
+
+# Example usage
+html_file_path = 'index.html'
+output_file_path = 'word_embeddings.json'
+word_embeddings = scrape_and_embed_words(html_file_path)
+
+if word_embeddings is not None:
+    save_word_embeddings_to_file(word_embeddings, output_file_path)
